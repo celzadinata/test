ab85cdd21693100b4fed9363b0586ac27122fec2
celzadinata <dinatacelza@gmail.com>
2025-05-29 01:03:05 +0700

    deployment

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["asset.kompas.com", "res.cloudinary.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
