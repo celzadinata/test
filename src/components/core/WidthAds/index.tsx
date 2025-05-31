6bd80f81fcd9dce1281339c6e9e969be1e7ff6c8
Garin Caesar <garincaesar69@gmail.com>
2025-05-31 20:12:47 +0700

    fix: ad bug

--- src/components/core/WidthAds/index.tsx
+++ src/components/core/WidthAds/index.tsx
@@ -3,7 +3,7 @@
 import dynamic from "next/dynamic";
 
 // Load AdBanner client-side only
-const AdBanner = dynamic(() => import("../AdBanner"), {
+const AdBannerComponent = dynamic(() => import("../Adbanner"), {
   ssr: false,
   loading: () => (
     <div className="bg-gray-200 h-38 flex items-center justify-center">
@@ -16,7 +16,7 @@ export default function WidthAds() {
   return (
     <div className="mb-5 mt-3 w-full bg-gray-100 p-4 rounded-md">
       <p className="text-xs text-gray-500 mb-2">Advertisement</p>
-      <AdBanner
+      <AdBannerComponent
         dataAdFormat="auto"
         dataFullWidthResponsive={true}
         dataAdSlot="5748426756"
