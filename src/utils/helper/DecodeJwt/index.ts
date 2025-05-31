5856be9edc8aad6e90e666a62215d5e3a64165f2
Garin Caesar <garincaesar69@gmail.com>
2025-05-31 22:33:17 +0700

    fix: add decodejwt

---
+++ src/utils/helper/DecodeJwt/index.ts
@@ -0,0 +1,10 @@
+export function decodeJwt(token: string): any {
+  try {
+    const base64Payload = token.split(".")[1];
+    const payload = Buffer.from(base64Payload, "base64").toString();
+    return JSON.parse(payload);
+  } catch (err) {
+    console.log("Error DecodeJWT");
+    return err;
+  }
+}
